725cf46a52bb5f192f42cef7894fb5d7a33274b0
AMANDA RUTHERFOORD <AMANDARUTH08@GMAIL.COM>
2018-06-25 14:04:07 -0500

    third commit

--- migrations/20180625114010_assassins_migrate.js
+++ migrations/20180625114010_assassins_migrate.js
@@ -3,11 +3,13 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('assassins', (table) => {
     table.increments();
     table.string('full_name').notNullable();
-    table.string('weapon');
-    table.integer('age');
-    table.integer('price');
-    table.decimal('rating', 3, 1);
-    table.integer('kills');
+    table.string('code_names').notNullable();
+    table.string('weapon').notNullable();
+    table.string('contact_info').notNullable();
+    table.integer('age').notNullable();
+    table.integer('price').notNullable();
+    table.decimal('rating', 2, 1).notNullable();
+    table.integer('kills').notNullable();
   })
 };
 
